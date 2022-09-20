b14c11c5117c29395c1c3aac4e1ce339d97e9fe1
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-20 21:20:32 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    for letter in str:
+
+        if ord('a') <= ord(letter) <= ord('z'):
+
+            letter = chr(ord(letter) - (ord('a') - ord('A')))
+
+        print("{:s}".format(letter),end="")
