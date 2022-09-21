a4fc6fd7d3813772815a65785ef4be6463bedfcf
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-21 12:25:28 +0000

    insert

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+listint_t temp, *new_node;
+
+
+new_node = malloc(sizeof(listint_t));
+
+if (new_node)
+{
+    temp = *head;
+    new_node->n = number;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+        new_node->next = NULL;
+        return (new_node);
+    }
+
+    *head = new_node;
+    new_node->next = temp;
+    return new_node;
+}
+
+return (NULL);
