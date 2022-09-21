b3016f9adc3a7e87093f1c3d47c701fdbe96968a
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-21 08:54:32 +0000

    print reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 
 for let in range(ord('z'), ord('a') - 1, -1):
 
-        print("{:s}".format((chr(let - 32) if let % 2 != 0 else chr(let))), end="")
+        print("{:s}".format((chr(let-32) if let%2 != 0 else chr(let))), end="")
