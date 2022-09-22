bc18629b9603071db3632ef358b2204655389674
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-22 20:25:28 +0000

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 0:
         print("0 arguments.")
-    if len(argv) == 1:
+    elif len(argv) == 1:
         print("1 arguments.")
     elif len(argv) > 0:
         print("{}: {}".format(len(argv) - 1, "arguments" if len(argv) > 2 else "argument"))
