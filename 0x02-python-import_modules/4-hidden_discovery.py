f3e73387511c99dfc334cd5d6eeb9837728f2a03
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-22 22:07:48 +0000

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    identifiers = dir(hidden_4)
+    for id in identifiers:
+        if id[:2] != "__":
+            print("{}".format(id))
