c7fdbf2eec1835e8b560a05b77485cd7b88f80b9
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-24 21:56:24 +0000

    new

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def new_in_list(my_list, idx, element):
+    if idx < 0 or id > (len(my_list) - 1):
+        return my_list
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+            return new_list
