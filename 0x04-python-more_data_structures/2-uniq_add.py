9d67b2966fe93ac8dc561acd32b001c0372210a9
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-29 01:28:00 +0000

    add unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_set = set()
-    my_set.update(my_list)
-    new_list = [elm for elm in my_set]
     res = 0
-    for i in new_list:
+    for i in set(my_list):
         res += i
-    res
+    return res  
