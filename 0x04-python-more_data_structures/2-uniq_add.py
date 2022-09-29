287c41bf77f12740e994764936f2d0fc4fcb10e0
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-29 01:16:00 +0000

    add uniqUE

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,4 +2,8 @@
 def uniq_add(my_list=[]):
     my_set = set()
     my_set.update(my_list)
-    return [elm for elm in my_set]
+    new_list = [elm for elm in my_set]
+    res = 0
+    for i in new_list:
+        res += i
+    res
