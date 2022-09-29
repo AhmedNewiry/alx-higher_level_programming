7f97be8726eb99bdb9b6fb64a5cbd71ddc3c83a1
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-29 00:31:56 +0000

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda list_t : [i**2 for i in list_t], matrix))
+    return list(map(lambda list_t: [i**2 for i in list_t], matrix))
