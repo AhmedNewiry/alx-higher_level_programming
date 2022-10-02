c310d1b661d7a180d263bd44e0b741b305c1f4ec
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-02 15:05:34 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    scores = [value for key, value in a_dictionary.items()]
-    scores.sort(reverse=True)
-    for key, value in a_dictionary.items():
-        if value == scores[0]:
-            return key
+    if a_dictionary:
+    	scores = [value for key, value in a_dictionary.items()]
+    	scores.sort(reverse=True)
+    	for key, value in a_dictionary.items():
+            if value == scores[0]:
+                return key
     return None
