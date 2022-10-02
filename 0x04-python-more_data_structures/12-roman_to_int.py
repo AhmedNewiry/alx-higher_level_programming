db8f629651606c144525620f319216c5d27e9f8c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-02 18:27:08 +0000

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bi/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
     res = 0
-
     for i in range(len(roman_string)):
 
         if roman.get(roman_string[i], -1) == -1:
-
             return 0
-        if i != len(roman_string) - 1 and 
+        if i != len(roman_string) - 1 and\
             roman[roman_string[i]] < roman[roman_string[i + 1]]:
             res += roman[roman_string[i]] * -1
         else:
