811c930271c440fc5f3e697a00039c408d46fc63
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-11 23:46:26 +0000

    squre

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,9 @@
 class Square:
     """class square with size attribute"""
     def __init__(self, size):
-    """initialize a square instance"""
+    """initialize a square instance
+    Args:
+	size (int): size of the square
+    """
     self.__size = size
 
