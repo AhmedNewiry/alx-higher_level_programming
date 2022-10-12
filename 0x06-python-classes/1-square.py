bc79466a36bb7b99ce7977ddfd9512ee0deacc14
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-12 14:35:48 +0000

    square1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,20 +2,13 @@
 
 
 """ defining a class."""
-
-
 class Square:
-
-
     """ represents a square class."""
 
     def __init__(self, size):
 
         """ initialization a new squre:
-
         Args:
-
             size (int): the square size
-
         """
         self.__size = size
