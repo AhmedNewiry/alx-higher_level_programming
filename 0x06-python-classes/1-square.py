23b3ab59f8e253df675da2624ae47fe4c6e6200d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-12 00:10:31 +0000

    size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,14 @@
 
 class Square:
     """class square with size attribute."""
+
     def __init__(self, size):
+
     	"""initialize a square instance.
+
     	Args:
 	    size (int): size of the square.
+
     	"""
         self.__size = size
 
