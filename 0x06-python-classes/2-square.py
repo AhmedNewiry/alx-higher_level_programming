f17e88cd537f9973feaeb661a74eb8e840faac09
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-12 00:36:37 +0000

    squre

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+""" defining a class."""
+
+
+from multiprocessing.sharedctypes import Value
+
+
+
+class Square:
+
+
+
+    """ represents a square class."""
+
+
+    def __init__(self, size = 0):
+
+        """ initialization a new squre:
+
+        Args:
+
+            size (int): the square size
+
+        """
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+
+            raise ValueError("size must be >= 0")    
+
+        self.__size = size
