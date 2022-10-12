b72b4a1aa7dec4401e0c0d40fff34a0a901c91ea
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-12 17:06:45 +0000

    sq6

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,79 +1,48 @@
 #!/usr/bin/python3
-
-
-
 """ defining a class."""
 
 
-
 class Square:
-
-
-
-
-
     """ represents a square class."""
-
-
 
     def __init__(self, size = 0):
 
         """ initialization a new squre:
 
         Args:
-
             size (int): the square size
 
-        """  
-
+        """
         self.size = size
 
-    @property    
-
+    @property
     def size(self):
-
         """ size attribue getter
 
         Returns: the size attribute
 
         """
-
         return self.__size
 
-
-
     @size.setter
-
-
-
     def size(self, value):
-
         """ size attribue setter;
 
         Args:
-
             value (int): the new size value
 
-
-
         """
-
         if not isinstance(value, int):
-
             raise TypeError("size must be an integer")
-
         elif value < 0:
-
             raise ValueError("size must be >= 0")  
-
         self.__size = value
 
     def area(self):
+        """ calculates the area of the square
 
-        """calculates the area of the square
-
-        Return: the square area
+        Returns: 
+            the square area
 
         """
-
         return (self.__size) ** 2
