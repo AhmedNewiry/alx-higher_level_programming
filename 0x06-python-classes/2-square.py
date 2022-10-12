6651a354f27cb9e6071487c5e0031cfa9fb5ec9c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-12 15:50:52 +0000

    sq2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,6 +3,7 @@
 
 """ defining a class."""
 
+
 class Square:
 
     """ represents a square class."""
@@ -17,7 +18,9 @@ class Square:
 
         """
         if not isinstance(size, int):
+
             raise TypeError("size must be an integer")
         elif size < 0:
+
             raise ValueError("size must be >= 0")    
 	    self.__size = size
