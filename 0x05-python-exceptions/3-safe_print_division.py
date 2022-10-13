8ddd00f2589d645bdae3a705ce47ca88f2e8f7ce
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-13 00:12:06 +0000

    safe

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         div = None
     finally:
-        print("Inside result: {:d}".format(sum))
+        print("Inside result: {}".format(sum))
     return div
