c3aa7ed5556d688aa4c95ef4ef390b24f2d399f1
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-25 23:09:47 +0000

    Checks the object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,4 +3,6 @@
 
 
 def is_same_class(obj, a_class):
-    """Return true if obj is the exact class a_class, otherwise false"""    return (type(obj) == a_class)
+    """Return true if obj is the exact class a_class, otherwise false
+    """
+    return (type(obj) == a_class)
