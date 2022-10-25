329dd8dbcc5393743960b1fd165ebeac41b0f0ec
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-25 23:00:05 +0000

    checks an the object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the is_name_class function"""
+
+
+def is_name_class(obj, a_class):
+    """Return true if obj is the exact class a_class, otherwise false"""
+    return (type(obj) == a_class)
