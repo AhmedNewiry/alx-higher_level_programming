185a12268672ea9e365d986e6a308d4e47c24e48
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-02-09 10:46:01 -0800

    api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const api = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(api, function (err, res, body) {
+  if (err) {
+  console.log(err);
+  }
+  console.log(res.title);
+});
