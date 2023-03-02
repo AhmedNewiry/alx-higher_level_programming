f68c41c60439b6ff22a29021395d24237e2daaae
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-02 11:24:26 -0800

    fix'

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -21,25 +21,25 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter of with property"""
-        if not isinstance(width, int):
-            raise TypeError('with must be an integer')
-        if width < 0:
-            raise ValueError('with must be >= 0')
+        if not isinstance(value, int):
+            raise TypeError('witdh must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
         """getter of height property"""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
         """setter for height property"""
-        if not isinstance(width, int):
-            raise TypeError('with must be an integer')
-        if width < 0:
-            raise ValueError('with must be >= 0')
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
 
     def area(self):
         """ a method that returns the rectangle area"""
