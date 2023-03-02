ede1e922f58676688044de139647ffddbb79bbec
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-02 13:08:41 -0800

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,36 +6,35 @@ import json
 
 
 class Base:
-	"""Base class definition"""
-	__nb_objects = 0
+    """Base class definition"""
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		"""initializer of base class"""
-		if id:
-			self.id = id
-		else:
-			id = type(Base).__nb__objects + 1
-			self.id = id
-
-	@staticmethod
-	def to_json_string(list_dictionaries):
-		"""returns the JSON string representation of list_dictionaries"""
-		if not list_dictionaries:
-			return []
+    def __init__(self, id=None):
+        """initializer of base class"""
+        if id:
+            self.id = id
         else:
-        	return json.dump(list_dictionaries)
+            d = type(Base).__nb__objects + 1
+            self.id = id
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if not list_dictionaries:
+            return []
+        else:
+            return json.dump(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-    	"""writes the JSON string representation of list_objs to a file"""
-    		f_name = f"{cls.__name__}.json"
-    		with open(f_name, 'w') as json_file:
-    			if not list_objs:
-    				json_file.write('[]')
-    			else:
-    				dict_list = [obj.to_dictionary() or obj in list_objs]
-    				json_file.write(cls.to_json_string(dict_list))
+        """writes the JSON string representation of list_objs to a file"""
+        f_name = f"{cls.__name__}.json"
+        with open(f_name, 'w') as json_file:
+            if not list_objs:
+                json_file.write('[]')
+            else:
+                dict_list = [obj.to_dictionary() or obj in list_objs]
+                json_file.write(cls.to_json_string(dict_list))
 
     @staticmethod
     def from_json_string(json_string):
@@ -46,25 +45,23 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-    	"""returns an instance with all attributes already set"""
-    	if dictionary:
-    		if cls.__name__ == 'Rectangle':
-    			dummy = cls(2, 4)
-    		elif cls.__name__ == 'Square':
-    			dummy = cls(2)
-    		dummy.update(**dictionary)
-    		return dummy
+        """returns an instance with all attributes already set"""
+        if dictionary:
+            if cls.__name__ == 'Rectangle':
+                dummy = cls(2, 4)
+            elif cls.__name__ == 'Square':
+                dummy = cls(2)
+            dummy.update(**dictionary)
+            return dummy
 
     @classmethod
     def load_from_file(cls):
-    	""" a method that returns a list of instances"""
-    	f_name = f"{cls.__name__}.json"
-    	try:
-    		with open(f_name, 'r') as json_file:
-    			dicts = cls.from_json_string(json_file.read())
-    			return [cls.create(**dict) for dict in dict]
+        """ a method that returns a list of instances"""
+        f_name = f"{cls.__name__}.json"
+        try:
+            with open(f_name, 'r') as json_file:
+                dicts = cls.from_json_string(json_file.read())
+                return [cls.create(**dict) for dict in dict]
 
-    	except IOError:
-    		return []
-
-    	
+        except IOError:
+            return []
