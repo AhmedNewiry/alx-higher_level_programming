ab9ea85f195573604caf57431f60012fbd438641
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-03 06:10:22 -0800

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
                                  passwd=passwd,
                                  db=db)
     db_cursor = connection.cursor()
-    command = db_cursor.execute("""SELECT c.id, c.name, s.name form
+    command = db_cursor.execute("""SELECT `c`.`id`, `c`.`name`, `s`.`name` form
                                    `states` AS s inner join `cities` AS c ON
-                                    s.id = c.state_id ORDER BY `c`.`id`""")
+                                    `s`.`id` = `c`.`state_id` ORDER BY `c`.`id`""")
     cities = db_cursor.fetchall()
     for city in cities:
         print(city)
