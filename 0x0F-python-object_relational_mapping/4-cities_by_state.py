349494c337d87a245b50023aba842644871b8c85
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-03 05:27:51 -0800

    cites

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+    connection = MySQLdb.connect(host=host,
+                                 port=port,
+                                 user=user,
+                                 passwd=passwd,
+                                 db=db)
+    db_cursor = connection.cursor()
+    command = db_cursor.execute("""SELECT * FROM `cities` ORDER BY `id`""")
+    cities = db_cursor.fetchall()
+    for city in citites:
+        print(city)
+    db_cursor.close()
+    connection.close()
