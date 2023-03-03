2d92bf948e985aa4f9893e3abbfaa312cc5c82d9
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-02 23:06:25 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
         if id:
             self.id = id
         else:
-            type(self).__nb_objects + 1
+            type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
     @staticmethod
