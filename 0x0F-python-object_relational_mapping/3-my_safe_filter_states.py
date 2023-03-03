4bed2075ec897f00d1c622ef43e97efbfd041baf
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-03-03 01:34:21 -0800

    pep

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
                                  db=db)
     argument = sys.argv[4]
     db_cursor = connection.cursor()
-    command = db_cursor.execute("""SELECT * FROM `states` where `name` = '{:s}'
-                                   ORDER BY `id`""".format(argument))
+    command = db_cursor.execute("""SELECT * FROM `states` where `name` = %(argument)s
+                                   ORDER BY `id`""", {'argument':argument})
     states = command.fetchall()
     for state in states:
         print state
