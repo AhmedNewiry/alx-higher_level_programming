949a6409124a2b999be571422734c8dd40350eb6
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-11-13 14:25:05 -0800

    Javascript warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const index in list) {
   console.log(list[index]);
 }
