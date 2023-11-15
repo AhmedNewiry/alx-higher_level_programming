1486c05dac53250b4678c7404aff4b75defdea4c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-11-15 15:04:37 -0800

    JavaScript

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const firstf = fs.readFileSync(process.argv[2], "utf-8");
-const secondf = fs.readFileSync(process.argv[3], "utf-8");
+const firstf = fs.readFileSync(process.argv[2]).toString();
+const secondf = fs.readFileSync(process.argv[3]).toString();
 fs.writeFileSync(process.argv[4], (firstf + secondf));
