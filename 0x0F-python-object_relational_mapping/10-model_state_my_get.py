c7ede12db1ce38ddc082b252998837c45c5cfff7
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-11-19 09:28:56 -0800

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -19,13 +19,12 @@ if __name__ == '__main__':
                                                                    passwd,
                                                                    host, port,
                                                                    db))
-    Base.metadata.create_ all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name == name)
-    if states:
-        for state in states:
-            print(state.id)
+    state = session.query(State).filter(State.name == name).one_or_none()
+    if state:
+        print(state.id)
     else:
         print("Not Found")  
     session.close()
