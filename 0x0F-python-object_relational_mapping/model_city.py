2073939c6bcd482c45ac5e35db9d00df1657d09c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-11-19 22:43:28 -0800

    fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 class City(Base):
+    """sqlalchemy model for cities table"""
     __tablename__ = 'cities'
     id = Column('id', Integer, primary_key=True, nullable=False)
     name = Column('name',String(128), nullable=False)
